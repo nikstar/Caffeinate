3ecb0e2d9a34e9ddb5a6f09fad3cc195fdf83e7a
Nikita Starshinov <nstarshinov@yandex.ru>
2017-11-15 23:55:18 +0300

    Initial Commit

---
+++ Caffeinate/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Caffeinate
+//
+//  Created by Nikita Starshinov on 15/11/2017.
+//  Copyright © 2017 Nikita Starshinov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
